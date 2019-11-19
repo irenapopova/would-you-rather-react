ff6efed8b0de2f7f307af45dce83888a5b7ba1fc
irenapopova <irenejpopova6@gmail.com>
2019-11-20 00:43:11 +0100

    Updates

---
+++ precache-manifest.1eb968f538733a07da95ce25eb989edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7592d905f3d41a3203c276c7af7c7577",
+    "url": "/would-you-rather-react/index.html"
+  },
+  {
+    "revision": "878e3ced1b545ad80247",
+    "url": "/would-you-rather-react/static/css/main.0dd51c21.chunk.css"
+  },
+  {
+    "revision": "6ea33ae4b5d451ad4fa8",
+    "url": "/would-you-rather-react/static/js/2.b35fe37d.chunk.js"
+  },
+  {
+    "revision": "878e3ced1b545ad80247",
+    "url": "/would-you-rather-react/static/js/main.beeca007.chunk.js"
+  },
+  {
+    "revision": "4ac6035988cdb7b8c90c",
+    "url": "/would-you-rather-react/static/js/runtime-main.801c655b.js"
+  }
+]);
